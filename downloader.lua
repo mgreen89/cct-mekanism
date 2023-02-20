46961a67573de87efb87bae7a0ce5aab0b262700
Matthew Green <matgreen@cisco.com>
2023-02-20 15:18:14 +0000

    Add a downloader script

---
+++ downloader.lua
@@ -0,0 +1,29 @@
+-- Download all the scripts here.
+
+local scripts = {
+    "displayutils",
+    "energy",
+    "energydisplay",
+    "fission",
+    "fusion",
+    "induction",
+    "sps",
+    "turbine",
+}
+
+local REPO = "https://raw.githubusercontent.com/mgreen89/cct-mekanism/main/"
+
+for _, s in pairs(scripts) do
+    local name = s .. ".lua"
+    local url = REPO .. name
+    d = http.get(url)
+    if d == nil then
+        print(string.format("Error getting %s", url))
+    else
+        local h = d.readAll()
+        f = fs.open(name, "w")
+        f.write(h)
+        f.close()
+        d.close()
+    end
+end
